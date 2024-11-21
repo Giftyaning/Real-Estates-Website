85c7da7048447458ad36d08d7b0332e282d62eb4
Gifty <giftyaningg@gmail.com>
2024-11-21 13:18:18 +0000

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div
       className="min-vh-100 mb-4 bg-cover bg-center align-items-center w-100 overflow-hidden"
       style={{
-        backgroundImage: "url('/header_img.png')",
+        backgroundImage: "url('/header.jpg')",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
@@ -18,7 +18,11 @@ const Header = () => {
       <NavBar />
 
       <div className=" container d-flex flex-column justify-content-center align-items-center vh-100 text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
-        <h2>Explore homes that fit your dreams</h2>
+        <h2
+          style={{
+            fontSize: "3.5em"
+          }}
+        >Explore homes that fit your dreams</h2>
         <div className="d-flex gap-3 mt-3">
           <a
             href="#Projects"
